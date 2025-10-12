18fb66cfa29b1c86cd602325d02ad0cab24c87e4
Menil Jotaniya <menil.nxt@gmail.com>
2025-10-12 13:50:22 +0530

    laying the foundation

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-//  ReactElement end of the day -> (Object).  ==> and this react Object is becomes (HTML) that broswer understand.
+import React from "react"; // "react is basically referse to react we have into node_module"
+import ReactDOM from "react-dom/client";
+
+/*    ReactElement end of the day -> (Object).  ==> and this react Object is becomes (HTML) that broswer understand.
 
 
-/*
+
     create nested element in React like :- div inside another div.
 
         <div id="perent">
@@ -15,11 +18,11 @@
             </div>
         </div>
       
-*/
+
 const perent = React.createElement(
     "div",{id: "perent"},
     [React.createElement("div", {id: "child"},
-    [React.createElement("h1",{},"I'am H1 tage in nested div!"), React.createElement("h2",{},"I'am H2 tage in nested div!"),React.createElement("h3",{},"I'am H3 tage in nested div!")]
+    [React.createElement("h1",{},"I'am H1 tage in nested div! 🚀"), React.createElement("h2",{},"I'am H2 tage in nested div!"),React.createElement("h3",{},"I'am H3 tage in nested div!")]
     ),React.createElement("div", {id: "child2"},
     [React.createElement("h1",{},"I'am H1 tage in nested div!"), React.createElement("h2",{},"I'am H2 tage in nested div!"),React.createElement("h3",{},"I'am H3 tage in nested div!")]
     )]
@@ -39,6 +42,60 @@ root.render(perent);
       // render work is take this heading object create h1 tag which does browser understand and put it inside root element.
       // rendering our heading (h1 tag) inside root
 
+*/
 
+// START LEARING REACT FROM HERE!!!!!!!!!!!!!!
 
+// React Element
+// older way to write React => you can also create react app with this older way of writing
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "this is react element 🦁"
+); // React.createElement => object => HTMLElement(render)
+console.log(heading);
 
+// JSX => JSX is new way to writing React. => but this react and jsx both are diffrent .
+// JSX => mearging html or js in one way .
+
+// how to create JSX element
+const jsxHeading = <h1 id="root">Menil</h1>;
+const jsxLink = <a href="">hello menil</a>;
+// JSX is not javascript inside HTML.  // both are differnt.  // JSX is a deiffrent independent syntax.
+console.log(jsxHeading);
+
+// how we create React COMPONENT
+// TWO type of React Component
+// 1 => React Class component => old => use js classes
+// 2 => React Functional component => new => use use js function
+
+const Title = () => (
+  <h1>
+    {"Hello JSX == > "}
+    Title Componenet --- Menil from React component without return but work is
+    same
+  </h1>
+); // with out return
+
+const HeadingComponent2 = () => {
+  return <h1>Menil from React component with return but work is same. </h1>;
+};
+
+//if you have maltiline than wrap inside ( ) (round breacket)
+
+// Component Composition.
+const HeadingComponent3 = () => (
+  <div className="Container">
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1 className="root">
+      Heading Cmpotonent inside title component --- Menil with multiple line
+      than wrap inside ( ) breacket
+    </h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // for displaying to browser we do ReactDOM.cerateRoot ()
+
+root.render(<HeadingComponent3 />); // render for component <function name/>
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # React
+
 Here you can see my react code and learn from it. let's start !!
+
+# Parcel
+
+- Dev Build
+- local server build and also host that local server for us.
+- HMR - Hot Module Replecement.
+- parcel uses File Watching Algorithm -> and this File Watching Algorithm is ( Written in C++ ).
+- Parcel is giving you (faste development expriance because is also doing Caching)
+- Caching throw -> Faster Builds.
+- Image Optimization
+- if We create Productation Build so parccel do Minification of our file also.
+- Bundling our file.
+- Compressing our file.
+- Consistent Hashing
+- Code Splliting
+- Differential Bundling - Support Older Browsers.
+- Diagnostic - beautiful understandeble Error.
+- Good Error handling Message - good sagession for handle any Error.
+- HTTPs.
+- Tree Shaking - Remove unuse code.
+- Different devlopment and production Bundles.
