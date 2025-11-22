be96a4ddce6479ba1601d1144be746529695d66e
Menil Jotaniya <menil.nxt@gmail.com>
2025-11-22 08:45:31 +0530

    swiggy app improve

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,8 +3,15 @@ import { CDN_URL } from "../utils/constants";
 const RestaurantCard = (props) => {
   let { resData } = props;
 
-  const { cloudinaryImageId, name, cuisines, avgRating, locality } =
-    resData?.info;
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    locality,
+    costForTwo,
+    sla,
+  } = resData?.info;
   return (
     <div className="res-card">
       <img alt="Image of Foods" src={CDN_URL + cloudinaryImageId} />
@@ -12,8 +19,9 @@ const RestaurantCard = (props) => {
       <div className="card-rating">
         <h5>{cuisines.join(", ")}</h5>
         <h5>{avgRating}</h5>
-        <h5>{resData.info.sla.deliveryTime} Minute</h5>
+        <h5>{sla.deliveryTime} Minute</h5>
         <h5>{locality}</h5>
+        <h5>{costForTwo}</h5>
       </div>
     </div>
   );
